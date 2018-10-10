c9ad2a024a920fbc952276b8b1b0501c15962861
Khuram Niaz <knshaikh@gmail.com>
2018-10-10 10:34:21 -0600

    toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent, SidenavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
