cf3ec79f33bcab9fe85f71fc1d152126c644b03e
Khuram Niaz <knshaikh@gmail.com>
2018-10-11 09:44:00 -0600

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { ActivitiesListComponent } from './components/activities-list/activities-list.component';
+import { ResourcesListComponent } from './components/resources-list/resources-list.component';
+import { ResourceDetailsComponent } from './components/resource-details/resource-details.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { ActivityDetailsComponent } from './components/activity-details/activity-details.component';
+import { ActivityFormComponent } from './components/activity-form/activity-form.component';
+import { ProjectFormComponent } from './components/project-form/project-form.component';
+import { ResourceFormComponent } from './components/resource-form/resource-form.component';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent, SidenavComponent],
+  declarations: [AppComponent, ToolbarComponent, SidenavComponent, ProjectsListComponent, ActivitiesListComponent, ResourcesListComponent, ResourceDetailsComponent, ProjectDetailsComponent, ActivityDetailsComponent, ActivityFormComponent, ProjectFormComponent, ResourceFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
