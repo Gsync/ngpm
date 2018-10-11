cd4e830ed95db284b21eff1cdcbf9224078c39c6
Khuram Niaz <knshaikh@gmail.com>
2018-10-11 10:04:53 -0600

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { ResourcesListComponent } from './components/resources-list/resources-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'projects', component: ProjectsListComponent },
+  { path: 'resources', component: ResourcesListComponent },
+  { path: '', redirectTo: '/projects', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
