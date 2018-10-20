34168e15aaea55b2a4abca52e6cfeef3aa61f89a
Khuram Niaz <knshaikh@gmail.com>
2018-10-20 13:56:20 -0600

    Resources table update: add sorting

--- src/app/components/resources-list/resources-list.component.ts
+++ src/app/components/resources-list/resources-list.component.ts
@@ -3,7 +3,7 @@ import { DataService } from 'src/app/services/data.service';
 import { Resource } from 'src/app/models/resource';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-resources-list',
@@ -25,11 +25,15 @@ export class ResourcesListComponent implements OnInit {
   datasource: MatTableDataSource<Resource>;
   @ViewChild(MatPaginator)
   paginator: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort;
+
   ngOnInit() {
     this.resources$ = this.dataService.getResources();
     this.resources$.subscribe(data => {
       this.datasource = new MatTableDataSource<Resource>(data);
       this.datasource.paginator = this.paginator;
+      this.datasource.sort = this.sort;
       this.store.dispatch({
         type: 'LOAD_RESOURCES',
         payload: data
